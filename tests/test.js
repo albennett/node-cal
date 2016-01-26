79b49355bd359a3e537ce4016eae907b765b20f4
Amy Lee <amyleeannebennett@gmail.com>
2016-01-26 16:50:27 -0600

    tests working for modifyYear & getDay

--- tests/test.js
+++ tests/test.js
@@ -1,37 +1,45 @@
 'use strict';
+
 const { expect } = require('chai');
 const { execSync } = require('child_process');
 
 describe('cal', () => {
   describe('CLI', () => {
-  it('should handle the current month', () => {
-    const goal = execSync('cal').toString();
-    const output = execSync('./cal.js').toString();
-  
-    expect(output).to.equal(goal);
+    it('should handle the current month', () => {
+      const goal = execSync('cal').toString();
+      const output = execSync('./cal.js').toString();
+
+      expect(output).to.equal(goal);
     });
   });
-  describe('Zellars congruence', () => {
-    // const zellars = require('../zellars.js');
-    describe('.modified_month', () => {
-      it('return 13 for January', () => {
-        const mod = zellars.modifiedMonth(2012, 01);
 
-        expect (mod).to.eqaul(13);
+  describe("Zeller's congruence", () => {
+     const zellers = require('../lib/zellers.js');
+
+    describe('.modifiedMonth', () => {
+      it('return 13 for January', () => {
+        expect(zellers.modifiedMonth(1)).to.equal(13);
       });
-      //2012, 02 === 14
-      //2012, 03 === 3
+      // 2 === 14
+      // 3 === 3
     });
-      //modifiedYear
-    //2000, 1 === 1999
-    //2012, 2 === 2011
-    //2013, 3, === 2013
-    //
-    //.calculate
-    //2014,3,2 === 1
-    //2012, 1, 1 === 1
-    //2012, 1, 12 === 5
-    //1799, 2, 1 === 6
-    //2000, 11, 1 === 4
+
+    describe('.modifiedYear', () => {
+      it('returns 2015 for Jan 2015', () => {
+        expect(zellers.modifiedYear(2015, 1)).to.equal(2014);
+      });
+      // 2016, 2 === 2015
+      // 2017, 3 === 2017
+    });
+
+    describe('.getDay', () => {
+      it('returns 2 (Tuesday) for March 1, 2016', () => {
+        expect(zellers.getDay(2016, 3, 1)).to.equal(2);
+      });
+      // 2000, 3, 1 === 3
+      // 2100, 3, 1 === 1
+      // 2200, 3, 2 === 0
+      // 2300, 3, 1 === 4
+    });
   });
 });

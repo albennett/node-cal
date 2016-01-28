fbb7878c500414432766e8e0b1abed32f5a0202e
Amy Lee <amyleeannebennett@gmail.com>
2016-01-28 11:14:22 -0600

    getting cal.js to connect with modules, and checking for arg lengths

--- cal.js
+++ cal.js
@@ -1,6 +1,31 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --harmony_destructuring
 'use strict';
-console.log('cal.js');
+const seeMonth = require('./lib/months.js').createMonth;
+const [,, ...args] = process.argv;
+console.log(args);
+
+if (args.length === 2) {
+  //const [month, year] = args;
+  console.log(seeMonth(args[0], args[1]));
+} else if (args.length === 1) {
+  console.log('im working on it,okk');
+  //const [year] = args;
+  //console.log(`generateYear(${year})`)
+} else {
+  console.log('dun broke it');
+  process.exit(64);
+}
+
+//if (year < 1753) {
+  //console.log('app does not work prior to 1753');
+//} else {
+  //console.log('cal');
+//}
 
 //const generateMonth = require('./lib/month);
+
 //console.log(generateMonth(2016,1));
+//
+//var monthView = require('./lib/month.js').generateMonthView;
+
+
